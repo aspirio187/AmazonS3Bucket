6081f74d602565154656c33e57c272f421bf883e
Soultan Hatsijev <soultan_98@hotmail.com>
2023-01-28 01:10:26 +0100

    Global namespace

--- AmazonS3Bucket/Program.cs
+++ AmazonS3Bucket/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace AmazonS3Bucket
+﻿namespace AmazonS3Bucket;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
